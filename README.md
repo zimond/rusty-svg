869ccf44b7eda6729da39c16de855cbb30f36685
Zimon Dai <daizhuoxian@gmail.com>
2021-05-27 15:29:53 +0800

    add inner_box(), crop() API

--- README.md
+++ README.md
@@ -5,7 +5,13 @@
   <a href="https://github.com/zimond/rusty-svg/actions/workflows/ci.yml"><img src="https://github.com/zimond/rusty-svg/actions/workflows/ci.yml/badge.svg?branch=main" alt="build status"></a>
 </p>
 
-An SVG toolkit based on resvg
+An SVG toolkit based on [resvg](https://github.com/RazrFalcon/resvg)
+
+This module is compiled to WASM and currently only supports Node.js
+
+Comparing with the backend ReSVG, this module removes text support as it requires complex
+font loading logic.
+
 ## Example
 
 Run the following command, it will convert [tiger.svg](tests/tiger.svg) to [tiger.png](tests/tiger.png)
@@ -14,6 +20,6 @@ Run the following command, it will convert [tiger.svg](tests/tiger.svg) to [tige
 node tests/index.js
 ```
 
-|  SVG   |  PNG  |
-|  ----  | ----  |
-| <img width="360" src="tests/tiger.svg" alt="Tiger.svg">  | <img width="360" src="tests/tiger.png" alt="Tiger.png"> |
+| SVG                                                     | PNG                                                     |
+| ------------------------------------------------------- | ------------------------------------------------------- |
+| <img width="360" src="tests/tiger.svg" alt="Tiger.svg"> | <img width="360" src="tests/tiger.png" alt="Tiger.png"> |
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,14 @@ use tiny_skia::Pixmap;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub struct BBox {
+    pub x: f64,
+    pub y: f64,
+    pub width: f64,
+    pub height: f64,
+}
+
+#[wasm_bindgen]
 pub struct RustySvg {
     tree: usvg::Tree,
 }
@@ -110,13 +118,17 @@ impl RustySvg {
                 if let Some(cap) = d.get(1) {
                     let mut data = cap.as_str().trim().split(' ').collect::<Vec<_>>();
                     assert_eq!(data.len(), 7);
-                    if data[1] == data[3] && data[2] == data[4] {
+                    let indicator = if data[1] == data[3] && data[2] == data[4] {
                         data.remove(4);
                         data.remove(3);
-                    }
+                        "Q"
+                    } else {
+                        "C"
+                    };
                     data.remove(0);
                     format!(
-                        " Q {}",
+                        " {} {}",
+                        indicator,
                         data.into_iter()
                             .map(|d| d.to_string())
                             .collect::<Vec<_>>()
@@ -128,4 +140,99 @@ impl RustySvg {
             })
             .to_string()
     }
+
+    pub fn inner_bbox(&self) -> BBox {
+        let mut min_point = Point::new(std::f64::MAX, std::f64::MAX);
+        let mut max_point = Point::new(0.0, 0.0);
+        for child in self.tree.root().children().skip(1) {
+            for node in child.descendants() {
+                self.node_bbox(node, &mut min_point, &mut max_point);
+            }
+        }
+        BBox {
+            x: min_point.x,
+            y: min_point.y,
+            width: max_point.x - min_point.x,
+            height: max_point.y - min_point.y,
+        }
+    }
+
+    pub fn crop(&mut self, bbox: &BBox) {
+        let mut node = self.tree.root();
+        let mut node = node.borrow_mut();
+        if let usvg::NodeKind::Svg(svg) = &mut *node {
+            let ratio_x = svg.size.width() / svg.view_box.rect.width();
+            let ratio_y = svg.size.height() / svg.view_box.rect.height();
+            svg.view_box.rect = usvg::Rect::new(bbox.x, bbox.y, bbox.width, bbox.height).unwrap();
+            svg.size = usvg::Size::new(bbox.width * ratio_x, bbox.height * ratio_y).unwrap();
+        }
+    }
+
+    fn node_bbox(&self, node: usvg::Node, min_point: &mut Point<f64>, max_point: &mut Point<f64>) {
+        match &*node.borrow() {
+            usvg::NodeKind::Path(p) => {
+                for seg in &p.data.0 {
+                    match seg {
+                        usvg::PathSegment::MoveTo { x, y } => {
+                            min_max_point(min_point, max_point, *x, *y)
+                        }
+                        usvg::PathSegment::LineTo { x, y } => {
+                            min_max_point(min_point, max_point, *x, *y)
+                        }
+                        usvg::PathSegment::CurveTo {
+                            x1,
+                            y1,
+                            x2,
+                            y2,
+                            x,
+                            y,
+                        } => {
+                            min_max_point(min_point, max_point, *x1, *y1);
+                            min_max_point(min_point, max_point, *x2, *y2);
+                            min_max_point(min_point, max_point, *x, *y)
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            usvg::NodeKind::Group(g) => {
+                if let Some(clippath) = g.clip_path.as_ref().and_then(|cp| self.node_by_id(cp)) {
+                    return self.node_bbox(clippath, min_point, max_point);
+                }
+                if let Some(mask) = g.mask.as_ref().and_then(|cp| self.node_by_id(cp)) {
+                    return self.node_bbox(mask, min_point, max_point);
+                }
+                for child in node.children() {
+                    self.node_bbox(child, min_point, max_point);
+                }
+            }
+            usvg::NodeKind::Image(image) => {
+                let (x, y) = image
+                    .transform
+                    .apply(image.view_box.rect.x(), image.view_box.rect.y());
+                let (x2, y2) = image
+                    .transform
+                    .apply(image.view_box.rect.right(), image.view_box.rect.bottom());
+                min_max_point(min_point, max_point, x, y);
+                min_max_point(min_point, max_point, x2, y2);
+            }
+            _ => {}
+        }
+    }
+
+    fn node_by_id(&self, id: &str) -> Option<usvg::Node> {
+        for node in self.tree.root().descendants() {
+            if id == node.borrow().id() {
+                return Some(node);
+            }
+        }
+        None
+    }
+}
+
+fn min_max_point(min_point: &mut Point<f64>, max_point: &mut Point<f64>, x: f64, y: f64) {
+    min_point.x = min_point.x.min(x);
+    min_point.y = min_point.y.min(y);
+    max_point.x = max_point.x.max(x);
+    max_point.y = max_point.y.max(y);
 }
