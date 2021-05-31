bed735fd8930b71170bf8b42e9202fc04aaff0e9
yisibl <percyley@qq.com>
2021-05-31 14:06:28 +0800

    test: add book.svg

--- tests/index.js
+++ tests/index.js
@@ -11,12 +11,20 @@ fs.writeFileSync(__dirname + '/tiger.png', pngBuffer);
 rusty.cubic_path_to_quad(0.5);
 fs.writeFileSync(__dirname + '/tiger-quadratic.svg', rusty.to_string());
 
-
 // test BBox
-const phoneSvg = fs.readFileSync(__dirname + '/tv.svg');
-const rusty2 = new RustySvg(phoneSvg.toString());
+const svg2 = fs.readFileSync(__dirname + '/tv.svg');
+const rusty2 = new RustySvg(svg2.toString());
 let bbox = rusty2.inner_bbox();
 console.log('bbox width: ', bbox.width);
 console.log('bbox height: ', bbox.height);
 rusty2.crop(bbox);
-fs.writeFileSync(__dirname + '/tv.png', rusty2.render(500/bbox.height));
+// 指定生成的 png 高度为 800
+fs.writeFileSync(__dirname + '/tv.png', rusty2.render(800/bbox.height));
+
+const svg3 = fs.readFileSync(__dirname + '/book.svg');
+const rusty3 = new RustySvg(svg3.toString());
+let bbox2 = rusty3.inner_bbox();
+console.log('bbox width: ', bbox2.width);
+console.log('bbox height: ', bbox2.height);
+rusty3.crop(bbox2);
+fs.writeFileSync(__dirname + '/book.png', rusty3.render());
