84f770dddb3d7d0d5bf53e36dc48893c751cc053
yisibl <percyley@qq.com>
2021-07-07 11:46:29 +0800

    test: Update Bbox test

--- tests/index.js
+++ tests/index.js
@@ -12,19 +12,24 @@ rusty.cubic_path_to_quad(0.5);
 fs.writeFileSync(__dirname + '/tiger-quadratic.svg', rusty.to_string());
 
 // test BBox
-const svg2 = fs.readFileSync(__dirname + '/tv.svg');
+const svg2 = fs.readFileSync(__dirname + '/heart.svg');
 const rusty2 = new RustySvg(svg2.toString());
 let bbox = rusty2.inner_bbox();
-console.log('bbox width: ', bbox.width);
-console.log('bbox height: ', bbox.height);
+console.log('bbox width: ', bbox.width, '浏览器 119.30921936035156');
+console.log('bbox height: ', bbox.height, '浏览器 87.28472137451172');
 rusty2.crop(bbox);
-// 指定生成的 png 高度为 800
-fs.writeFileSync(__dirname + '/tv.png', rusty2.render(800/bbox.height));
 
-const svg3 = fs.readFileSync(__dirname + '/book.svg');
-const rusty3 = new RustySvg(svg3.toString());
-let bbox2 = rusty3.inner_bbox();
-console.log('bbox width: ', bbox2.width);
-console.log('bbox height: ', bbox2.height);
-rusty3.crop(bbox2);
-fs.writeFileSync(__dirname + '/book.png', rusty3.render());
+
+
+
+console.log(rusty2.to_string());
+// 指定生成的 png 高度为 800
+fs.writeFileSync(__dirname + '/heart.png', rusty2.render(500));
+
+// const svg3 = fs.readFileSync(__dirname + '/book.svg');
+// const rusty3 = new RustySvg(svg3.toString());
+// let bbox2 = rusty3.inner_bbox();
+// console.log('bbox width: ', bbox2.width);
+// console.log('bbox height: ', bbox2.height);
+// rusty3.crop(bbox2);
+// fs.writeFileSync(__dirname + '/book.png', rusty3.render());
